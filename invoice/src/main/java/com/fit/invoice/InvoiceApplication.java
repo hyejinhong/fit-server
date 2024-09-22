03d3f2e6470fac21a9e64ac5bd57aa87579e146f
Hyejin Hong <hhj732@naver.com>
2024-09-22 14:46:42 +0900

    pid 파일 생성

--- invoice/src/main/java/com/fit/invoice/InvoiceApplication.java
+++ invoice/src/main/java/com/fit/invoice/InvoiceApplication.java
@@ -2,12 +2,14 @@ package com.fit.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class InvoiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InvoiceApplication.class, args);
+		SpringApplication app = new SpringApplication(InvoiceApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
-
 }
